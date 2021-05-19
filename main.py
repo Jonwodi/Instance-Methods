7a03b3bab33a30046978918c7b588063d68e69bd
Jordan Onwodi <>
2021-05-19 15:54:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# class Student:
+#   def __init__(self, scores = []):
+#     self.scores = scores
+
+#   def avg(self):
+#     return round(sum(self.scores) / len(self.scores))
+
+# object / instance
+
+# kings = Student(scores = [2,3,4,5,2,3])
+# mrOrganik = Student(scores = [2,3,4,5,2,13])
+
+# print(kings.avg())
+# print(mrOrganik.avg())
